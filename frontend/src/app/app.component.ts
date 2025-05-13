d5ec71b4735a7214e18eb0c314080e2dcf5d9952
Jose Delgado <jluidroma@udenar.edu.co>
2025-05-12 22:39:53 -0500

    se actualizaron cambios en el componente home

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,13 +4,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
-import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeaderComponent,HomeComponent, CarruselComponent, RegistroComponent, LoginComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,HomeComponent, CarruselComponent,RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
