05967f80de42f5f5a3816a1cfa0dea45a8439c0e
Yeffer Gomez <yegoalvear@gmail.com>
2025-05-26 12:08:35 -0500

    correcion de errores en main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from './environments/environment';
 
+// Si appConfig tiene más configuraciones, las importamos
+import { appConfig } from './app/app.config';
+
 bootstrapApplication(AppComponent, {
   providers: [
+    ...appConfig.providers, // <-- Añadimos los providers anteriores
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth())
